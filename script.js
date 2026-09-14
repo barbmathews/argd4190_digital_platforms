fac1c1adc15daeb99615c385e7f35e3ac299d034
bam02207-gif <barb_mathews3150@yahoo.com>
2026-09-14 13:51:21 -0400

    9/14-1

---
+++ script.js
@@ -0,0 +1,13 @@
+let numInt=5
+let numFloat=2.5
+
+let student= {
+    name: "Barbara"
+    major: "Graphic Design"
+    age: "21"
+}
+
+document.getElementById("para").style.color = "green"
+
+let myElement = document.getElementById("para")
+myElement.style.fontSize = "2rem"
